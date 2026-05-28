e5dc44cfa00780731bb9bd5f84a605596b4ce8cb
poshpinkmarketing <hello@poshpinkmarketing.com>
2026-05-28 09:06:06 -0700

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: { include: ['jspdf'] },
+  build: { commonjsOptions: { include: [/jspdf/, /node_modules/] } }
+})
